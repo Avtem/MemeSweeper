3f56c520f3c3304d750354cb8231ff4e4dc032ec
Avtem <artemsav4465@gmail.com>
2021-10-23 08:26:30 +0300

    Tile class finished!

---
+++ Engine/Tile.cpp
@@ -0,0 +1,38 @@
+#include "Tile.h"
+
+#define lmbUp Mouse::Event::Type::LRelease
+#define rmbUp Mouse::Event::Type::RRelease
+
+bool Tile::parseMouse(Mouse::Event::Type mouseEv)
+{
+    if(revealed)
+        return false;
+
+    if(mouseEv == lmbUp)
+    {
+        switch (obj)
+        {
+            case ObjT::Meme:
+                drawState = DrawSt::FatalMeme;
+                revealed = true;
+                return true;
+            case ObjT::Void:
+            case ObjT::Number:
+                revealed = true;
+                break;
+        }
+    }
+    else if (mouseEv == rmbUp)
+    {
+        // toggle flag
+        drawState = drawState == DrawSt::Normal ? DrawSt::Flag : DrawSt::Normal;
+    }
+
+    return false;
+}
+
+void Tile::revealForLoser()
+{
+    if(false == revealed && drawState == DrawSt::Flag)
+        drawState = obj == ObjT::Meme ? DrawSt::CorrectFlag : DrawSt::WrongFlag;
+}
---
+++ Engine/Tile.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "SpriteCodex.h"
+#include "Mouse.h"
+
+enum class ObjT // object type (the actual thing that resides in the tile)
+{
+    Void = 0,
+    Number, // the tile contains number of adj.memes
+    Meme
+};
+enum class DrawSt // drawing state
+{
+    Normal,
+    Flag,
+    // after losing:
+    FatalMeme,
+    CorrectFlag,
+    WrongFlag,
+    HiddenMeme
+};
+
+class Tile
+{
+public:
+    int numOfAdjMemes = -1; // if no number!
+//////////// member functions
+    bool parseMouse(Mouse::Event::Type mouseEv); // returns TRUE if you hit a meme
+    void revealForLoser();
+
+private:
+    ObjT obj = ObjT::Void;
+    DrawSt drawState = DrawSt::Normal;
+    bool revealed = false;
+
+    static constexpr int dimension = SpriteCodex::tileSize;
+};
+
